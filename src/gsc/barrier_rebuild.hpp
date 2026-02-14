f85dc236f169c6174e3ce249b26414250eaae9d8
kane9287 <99044747+kane9287@users.noreply.github.com>
2026-02-14 15:13:03 -0800

    Add barrier rebuild header file

---
+++ src/gsc/barrier_rebuild.hpp
@@ -0,0 +1,13 @@
+// barrier_rebuild.hpp
+// Header for native barrier rebuild system
+
+#pragma once
+
+namespace gsc
+{
+	namespace barrier
+	{
+		// Initialize barrier rebuild hooks and custom methods
+		void init();
+	}
+}
